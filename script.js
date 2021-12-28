5a501436919b310f1902b1ca3c08031bd2fc8b2c
Ph-uad <temidayo26j@gmail.com>
2021-12-28 02:57:40 +0100

    commit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let menuCards = ''
+let array = [];
+const breakfast =  document.querySelector('.breakfast-list');
+const lunch =  document.querySelector('.lunch-list');
+const dinner =  document.querySelector('.dinner-list');
 
 const filteringJsonData = async () => {
-    const response = await axios.request('./data.json');
-    menuTemplate(response.data);
+  const response = await axios.request('./data.json');
+  menuTemplate(response.data);
 }
 
-const generateHTML = (item) =>{
-  return  `
+const generateHTML = (item) => {
+  return `
   <li id=${item.id} class="card">
     <h3 class="subheading card-header"><span class="card-image">${item.image}</span><span>${item.productName}</span></h3>
   </li>
@@ -14,14 +17,23 @@ const generateHTML = (item) =>{
 }
 
 const menuTemplate = (a) => {
+  let  menuCards = ' '
 
-    a.map(item => {
-        menuCards = document.createElement('sub');
-        menuCards.innerHTML = generateHTML(item);
+   a.map(item => {
+    menuCards= document.createElement('sub');
+    menuCards.innerHTML = generateHTML(item);
 
-       document.querySelector('.breakfast-list').appendChild(menuCards);
-    })
-    
+    array.push(menuCards)
+  })
+  joker(breakfast);
+  joker(lunch);
+  joker(dinner);
+}
+
+const joker = (who) =>{
+  for (let i of array){
+   who.appendChild(i.cloneNode(1));
+  }
 }
 
 filteringJsonData();
